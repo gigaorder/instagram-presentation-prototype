8f696c04229e90f8a071f6f3d0bd7bfc2d85055c
LongNC <long.nguyen2@gigasource.io>
2019-10-02 14:55:01 +0700

    Refactor permission util, move to util package

--- app/src/main/java/com/demo/instagram_presentation/util/PermissionUtil.java
+++ app/src/main/java/com/demo/instagram_presentation/util/PermissionUtil.java
@@ -1,4 +1,4 @@
-package com.demo.instagram_presentation.hotfix_plugin;
+package com.demo.instagram_presentation.util;
 
 import android.Manifest;
 import android.app.Activity;
@@ -8,29 +8,30 @@ import android.os.Build;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.demo.instagram_presentation.InstagramApplicationContext;
+import com.demo.instagram_presentation.activity.MainActivity;
+
 import lombok.Setter;
 
 public class PermissionUtil {
-    @Setter
-    private static Activity activity;
 
     public static void askForStoragePermissions() {
         if (Build.VERSION.SDK_INT < 23) {
             return;
         }
         if (!hasStoragePermissions()) {
-            ActivityCompat.requestPermissions(activity, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+            ActivityCompat.requestPermissions(MainActivity.self, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
         }
     }
 
     public static boolean hasStoragePermissions() {
         if (Build.VERSION.SDK_INT >= 16) {
-            final int readStorageGranted = ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
-            final int writeStorageGranted = ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            final int readStorageGranted = ContextCompat.checkSelfPermission(InstagramApplicationContext.context.getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+            final int writeStorageGranted = ContextCompat.checkSelfPermission(InstagramApplicationContext.context.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
             return readStorageGranted == PackageManager.PERMISSION_GRANTED && writeStorageGranted == PackageManager.PERMISSION_GRANTED;
         } else {
             // When SDK_INT is below 16, READ_EXTERNAL_STORAGE will also be granted if WRITE_EXTERNAL_STORAGE is granted.
-            final int res = ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            final int res = ContextCompat.checkSelfPermission(InstagramApplicationContext.context.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
             return res == PackageManager.PERMISSION_GRANTED;
         }
     }
