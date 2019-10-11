7928c3d04bc99005cddf2315c12075eed2f98100
LongNC <long.nguyen2@gigasource.io>
2019-10-11 18:52:33 +0700

    hide navigation bar

--- app/src/main/java/com/demo/instagram_presentation/activity/MainActivity.java
+++ app/src/main/java/com/demo/instagram_presentation/activity/MainActivity.java
@@ -9,6 +9,7 @@ import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.provider.Settings.Secure;
@@ -181,5 +182,17 @@ public class MainActivity extends AppCompatActivity {
         BroadcastReceiverUtil.unregisterReceiver(this, wifiScanResultReceiver);
         BroadcastReceiverUtil.unregisterReceiver(this, loginFailedReceiver);
         webServer.stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility
+                ( View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY );
     }
 }
