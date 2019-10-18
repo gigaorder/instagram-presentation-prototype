810dbfa7c5a64acbf2eb80c4f4c3d2adac0b5507
Lam Phan Dang <lam.phan@gigasouce.io>
2019-10-18 14:48:06 +0700

    test

--- script/create-patch-files.sh
+++ script/create-patch-files.sh
@@ -5,7 +5,6 @@ FOLDER_LIST=("${FOLDER_LIST[@]##*/}")
 
 for VERSION_TO_PATCH in "${FOLDER_LIST[@]}"
 do
-  echo ${VERSION_TO_PATCH}
   mkdir -p ./originalBuild
   rm ./originalBuild/app.apk
   cp /var/jenkins_home/files/feed2wall/apk/${VERSION_TO_PATCH}/app.apk ./originalBuild/app.apk
