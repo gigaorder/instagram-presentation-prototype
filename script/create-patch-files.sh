8ede918cdf857fa95074e417afcc2a20ecfd1024
Lam Phan Dang <lam.phan@gigasouce.io>
2019-10-18 14:00:34 +0700

    Update create-patch-files.sh

--- script/create-patch-files.sh
+++ script/create-patch-files.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 FOLDER_LIST=`ls -d */`
+TOPIC="instagramPatching"
 
 for FOLDER in "${FOLDER_LIST[@]}"
 do
   VERSION_TO_PATCH=`echo ${FOLDER} | sed 's/\///g'`
   mkdir -p ./originalBuild
+  rm ./originalBuild/app.apk
   cp /var/jenkins_home/files/feed2wall/apk/${VERSION_TO_PATCH}/app.apk ./originalBuild/app.apk
   ./gradlew tinkerPatchDebug
-  ./copyPatch -v ${VERSION_TO_PATCH} -t instagramPatching
+  ./copyPatch -v ${VERSION_TO_PATCH} -t ${TOPIC}
 done
